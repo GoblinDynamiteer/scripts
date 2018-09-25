c5ab27351c9807cad8317cc9ca49c6bba9a7083a
Johan Kämpe <johan.kampe@chargestorm.se>
2018-09-25 14:41:11 +0200

    goto_loc_gen: work gotos
    Signed-off-by: Johan Kämpe <johan.kampe@chargestorm.se>

--- goto_loc_gen.py
+++ goto_loc_gen.py
@@ -23,20 +23,15 @@ LOCS = {"dl":    {"destinations": [os.path.join(HOME, "downloads"),
                                    os.path.join(HOME, "Downloads"),
                                    os.path.join(os.sep, "volume2", "DATA", "Temp", "Downloads")]},
         "code":  {"destinations": [os.path.join(HOME, "code")]},
-        "script":  {"destinations": [os.path.join(HOME, "scripts")]},
-        "film":  {"destinations": [os.path.join(HOME, "smb", "film"),
-                                   os.path.join(os.sep, "volume2", "FILM")],
-                  "alternatives": ["movies", "mov"]},
-        "tv":    {"destinations": [os.path.join(HOME, "smb", "tv"),
-                                   os.path.join(os.sep, "volume2", "TV")]},
-        "dsdl":  {"destinations": [os.path.join(HOME, "smb", "data", "Temp", "Downloads")],
-                  "alternatives": ["dlds", "datadl"]},
-        "data":  {"destinations": [os.path.join(HOME, "smb", "data")]},
-        "smb":   {"destinations": [os.path.join(HOME, "smb")]},
-        "misc":  {"destinations": [os.path.join(HOME, "smb", "misc")]},
-        "audio": {"destinations": [os.path.join(HOME, "smb", "audio")]},
+        "script":  {"destinations": [os.path.join(HOME, "script")]},
+        "py":  {"destinations": [os.path.join(HOME, "script", "python")]},
+        "cm":  {"destinations": [os.path.join(HOME, "cs", "chargemanager")]},
+        "cs":  {"destinations": [os.path.join(HOME, "cs")]},
+        "om":  {"destinations": [os.path.join(HOME, "cs", "outletmanager")]},
+        "hcc":  {"destinations": [os.path.join(HOME, "cs", "hcc")]},
+        "drive2": {"destinations": [os.path.join(HOME, "gdrive")]},
         "drive": {"destinations": [os.path.join(HOME, "hd2", "gdrive"),
-                                   os.path.join(HOME, "gdrive")]}
+                                   os.path.join(HOME, "gdrive-personal")]}
         }
 
 save(LOCS, JSON_FILE_PATH)
