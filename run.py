adf6d8dae6f23bff71198d816d21c3f17cafd72e
Johan Kämpe <kampe.johan@gmail.com>
2018-10-14 10:57:45 +0200

    run: update to_color_str usage, add arg to hide info
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 '''Run commands on local system'''
 
-import os
 import subprocess
 import str_o
 
-PRINT = str_o.PrintClass(os.path.basename(__file__))
+CSTR = str_o.to_color_str
 
 
-def local_command(command, hide_output=True):
+def local_command(command, hide_output: bool = True, print_info: bool = True):
     "Run a local command"
-    print(f"{PRINT.color_str('local', 'blue')} : {command}")
+    if print_info:
+        print(f"{CSTR('local', 'blue')} : {command}")
     if hide_output:
         ret = subprocess.run(command, shell=True,
                              stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
