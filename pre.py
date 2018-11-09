01f7e6a11b5cf684aa96ac9f722290f80579fbcd
Johan Kämpe <kampe.johan@gmail.com>
2018-11-10 00:17:28 +0100

    pre: add page count 50
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- pre.py
+++ pre.py
@@ -10,7 +10,8 @@ PARSER = argparse.ArgumentParser(description='Pre Search')
 PARSER.add_argument('query', type=str, help='Search query')
 ARGS = PARSER.parse_args()
 
-JSON_RESPONSE = requests.get(f"https://predb.ovh/api/v1/?q={ARGS.query}")
+JSON_RESPONSE = requests.get(
+    f"https://predb.ovh/api/v1/?q={ARGS.query}&count=50")
 DATA = json.loads(JSON_RESPONSE.text)
 
 ROWS = DATA['data']['rows']
