cddb7de2cf156fcdb08e2e85ff35278e5c77fee3
Johan Kämpe <kampe.johan@gmail.com>
2018-11-10 00:11:22 +0100

    pre: add pre search script
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

---
+++ pre.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.6
+
+""" pre search """
+
+import argparse
+import json
+import requests
+
+PARSER = argparse.ArgumentParser(description='Pre Search')
+PARSER.add_argument('query', type=str, help='Search query')
+ARGS = PARSER.parse_args()
+
+JSON_RESPONSE = requests.get(f"https://predb.ovh/api/v1/?q={ARGS.query}")
+DATA = json.loads(JSON_RESPONSE.text)
+
+ROWS = DATA['data']['rows']
+
+for row in ROWS:
+    print(row['name'])
