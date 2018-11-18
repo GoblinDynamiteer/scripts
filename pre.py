30658c19073a76a22ff17f891c1b04f3fd8931bf
Johan Kämpe <kampe.johan@gmail.com>
2018-11-18 21:14:08 +0100

    pre: use a method for searching
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- pre.py
+++ pre.py
@@ -6,18 +6,24 @@ import argparse
 import json
 import requests
 
+
+def pre_search(query: str) -> list:
+    """ runs a pre search, returns list matched names """
+    json_response = requests.get(
+        f"https://predb.ovh/api/v1/?q={query}&count=50")
+    data = json.loads(json_response.text)
+    rows = data['data']['rows']
+
+    return [row['name'] for row in rows]
+
+
 PARSER = argparse.ArgumentParser(description='Pre Search')
 PARSER.add_argument('query', type=str, help='Search query')
 PARSER.add_argument('--suffix', type=str, help='add suffix', default=None)
 ARGS = PARSER.parse_args()
 
-JSON_RESPONSE = requests.get(
-    f"https://predb.ovh/api/v1/?q={ARGS.query}&count=50")
-DATA = json.loads(JSON_RESPONSE.text)
-
-ROWS = DATA['data']['rows']
 
 suffix = ARGS.suffix if ARGS.suffix else ""
 
-for row in ROWS:
-    print(row['name'] + suffix)
+for name in pre_search(ARGS.query):
+    print(name + suffix)
