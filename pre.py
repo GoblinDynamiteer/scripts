bd7555f74b70a67d5cf336efc5cfb5501a7d1f0a
Johan Kämpe <kampe.johan@gmail.com>
2018-11-24 11:12:09 +0100

    pre: add pre_search_from_file method
    currently very rudamentary
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- pre.py
+++ pre.py
@@ -17,6 +17,26 @@ def pre_search(query: str) -> list:
     return [row['name'] for row in rows]
 
 
+def pre_search_from_file(file_name: str) -> str:
+    """ runs a pre search, return best match if possible """
+    split_filename = file_name.split('.')
+    result = []
+    trim_right = -1
+    query_list = split_filename
+    while not result and query_list:
+        query_list = split_filename[:trim_right]
+        query = f'"{" ".join(query_list)}"'
+        if not query:
+            break
+        result = pre_search(query)
+        if result:
+            break
+        trim_right -= 1
+    if result:
+        return result[0]
+    return ""
+
+
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(description='Pre Search')
     PARSER.add_argument('query', type=str, help='Search query')
