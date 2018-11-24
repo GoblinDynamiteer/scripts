f39375e4f2f2dcf1ced32aa4bf1339c36267ccbe
Johan Kämpe <kampe.johan@gmail.com>
2018-11-24 12:49:02 +0100

    pre: improve file search
    by trimming from left, right, both ends of query list
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- pre.py
+++ pre.py
@@ -17,23 +17,46 @@ def pre_search(query: str) -> list:
     return [row['name'] for row in rows]
 
 
+def _query_list_trim(queries: list, left: int = 0, right: int = 0):
+    list_len = len(queries)
+    if not right:
+        right = list_len
+    if left > list_len or abs(right) > list_len:
+        return []
+    if left > abs(right):
+        return []
+    return queries[left:right]
+
+
 def pre_search_from_file(file_name: str) -> str:
     """ runs a pre search, return best match if possible """
     split_filename = file_name.split('.')
     result = []
-    trim_right = -1
-    query_list = split_filename
-    while not result and query_list:
-        query_list = split_filename[:trim_right]
-        query = f'"{" ".join(query_list)}"'
-        if not query:
+    trim_right = 0
+    trim_left = 0
+    query_lists = {"left": split_filename,
+                   "right": split_filename, "both": split_filename}
+    while True:
+        query_lists["left"] = _query_list_trim(split_filename, left=trim_left)
+        query_lists["right"] = _query_list_trim(
+            split_filename, right=trim_right)
+        query_lists["both"] = _query_list_trim(
+            split_filename, left=trim_left, right=trim_right)
+
+        if not query_lists["right"] and not query_lists["left"]:
             break
-        result = pre_search(query)
-        if result:
-            break
-        trim_right -= 1
-    if result:
-        return result[0]
+
+        for _, query_list in query_lists.items():
+            if not query_list:
+                continue
+            query = f'"{" ".join(query_list)}"'
+            if not query:
+                continue
+            result = pre_search(query)
+            if result:
+                return result[0]
+            trim_right -= 1
+            trim_left += 1
     return ""
 
 
