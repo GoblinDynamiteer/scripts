616de388bc7c87c9a8c4ee785e7656cbda928f46
Johan Kämpe <kampe.johan@gmail.com>
2018-11-25 11:41:08 +0100

    ripper: refactor sr into own method, add arguments to script
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- ripper.py
+++ ripper.py
@@ -1,33 +1,52 @@
 #!/usr/bin/python3.6
 
+" Rip things from various websites, call script with URL"
+
+import argparse
 import subprocess
 from urllib.request import urlopen
+
 from bs4 import BeautifulSoup as bs
+
 import rename
 
 
-URL_ROOT = "https://sverigesradio.se"
-URL = f"{URL_ROOT}/snedtanktmedkallelind"
-PAGE = urlopen(URL).read()
-SOUP = bs(PAGE, "lxml")
-
-LINKS = [(a.get('href'), a.text)
-         for a in SOUP.find_all('a', href=True, text=True)]
+def _make_soup(url: str):
+    page = urlopen(url).read()
+    soup = bs(page, "lxml")
+    return soup
 
 
-def filter_episodes_from_linklist(links):
-    ''' Filter out episodes '''
-    return [(f"{URL_ROOT}{link}", title) for link, title in links if "avsnitt" in link]
+def _sveriges_radio(url: str):
+    print(f"trying to download sr episodes at {url}")
+    soup = _make_soup(url)
+    links = [(a.get('href'), a.text)
+             for a in soup.find_all('a', href=True, text=True)]
+
+    episodes = [(f"https://sverigesradio.se{link}", title)
+                for link, title in links if "avsnitt" in link]
+
+    for link, title in episodes:
+        print(f"{link}")
+        episode_page = urlopen(link).read()
+        episode_soup = bs(episode_page, "lxml")
+        episode_links = [a.get('href')
+                         for a in episode_soup.find_all('a', href=True)]
+        for episode_link in episode_links:
+            if episode_link.endswith(".mp3"):
+                file_name = f"{rename.rename_string(title)}.mp3"
+                subprocess.run(
+                    f"wget -O $HOME/Downloads/{file_name} https:{episode_link}", shell=True)
+                break
 
 
-for link, title in filter_episodes_from_linklist(LINKS):
-    episode_page = urlopen(link).read()
-    episode_soup = bs(episode_page, "lxml")
-    episode_links = [a.get('href')
-                     for a in episode_soup.find_all('a', href=True)]
-    for episode_link in episode_links:
-        if episode_link.endswith(".mp3"):
-            file_name = f"{rename.rename_string(title)}.mp3"
-            subprocess.run(
-                f"wget -O $HOME/Downloads/{file_name} https:{episode_link}", shell=True)
-            break
+if __name__ == "__main__":
+    METHODS = [('sverigesradio', _sveriges_radio)]
+
+    PARSER = argparse.ArgumentParser(description='ripper')
+    PARSER.add_argument('url', type=str, help='URL')
+    ARGS = PARSER.parse_args()
+
+    for hit, method in METHODS:
+        if hit in ARGS.url:
+            method(ARGS.url)
