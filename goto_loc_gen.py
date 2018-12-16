312ebfeede55acce31bc1f2b27a543229b6f736d
Johan Kämpe <johan.kampe@chargestorm.se>
2018-12-16 18:08:47 +0100

    goto_loc_gen: use OPJ variable for os.path.join
    Signed-off-by: Johan Kämpe <johan.kampe@chargestorm.se>

--- goto_loc_gen.py
+++ goto_loc_gen.py
@@ -15,30 +15,31 @@ def save(data, json_file):
         jf.write(json_string)
 
 
+OPJ = os.path.join
 HOME = os.path.expanduser("~")
 SCRIPT_DIR = os.path.realpath(__file__)
-JSON_FILE_PATH = os.path.join(os.path.dirname(SCRIPT_DIR), "goto_locs.json")
+JSON_FILE_PATH = OPJ(os.path.dirname(SCRIPT_DIR), "goto_locs.json")
 
-LOCS = {"dl":    {"destinations": [os.path.join(HOME, "downloads"),
-                                   os.path.join(HOME, "Downloads"),
-                                   os.path.join(os.sep, "volume2", "DATA", "Temp", "Downloads")]},
-        "code":  {"destinations": [os.path.join(HOME, "code")]},
-        "script":  {"destinations": [os.path.join(HOME, "scripts")]},
-        "film":  {"destinations": [os.path.join(HOME, "smb", "film"),
-                                   os.path.join(os.sep, "volume2", "FILM")],
+LOCS = {"dl":    {"destinations": [OPJ(HOME, "downloads"),
+                                   OPJ(HOME, "Downloads"),
+                                   OPJ(os.sep, "volume2", "DATA", "Temp", "Downloads")]},
+        "code":  {"destinations": [OPJ(HOME, "code")]},
+        "script":  {"destinations": [OPJ(HOME, "scripts")]},
+        "film":  {"destinations": [OPJ(HOME, "smb", "film"),
+                                   OPJ(os.sep, "volume2", "FILM")],
                   "alternatives": ["movies", "mov"]},
-        "tv":    {"destinations": [os.path.join(HOME, "smb", "tv"),
-                                   os.path.join(os.sep, "volume2", "TV")]},
-        "dsdl":  {"destinations": [os.path.join(HOME, "smb", "data", "Temp", "Downloads")],
+        "tv":    {"destinations": [OPJ(HOME, "smb", "tv"),
+                                   OPJ(os.sep, "volume2", "TV")]},
+        "dsdl":  {"destinations": [OPJ(HOME, "smb", "data", "Temp", "Downloads")],
                   "alternatives": ["dlds", "datadl"]},
-        "data":  {"destinations": [os.path.join(HOME, "smb", "data")]},
-        "nzb":   {"destinations": [os.path.join(HOME, "nzbget")]},
-        "dotfiles":   {"destinations": [os.path.join(HOME, "dotfiles")], 'alternatives' : ['df']},
-        "smb":   {"destinations": [os.path.join(HOME, "smb")]},
-        "misc":  {"destinations": [os.path.join(HOME, "smb", "misc")]},
-        "audio": {"destinations": [os.path.join(HOME, "smb", "audio")]},
-        "drive": {"destinations": [os.path.join(HOME, "hd2", "gdrive"),
-                                   os.path.join(HOME, "gdrive")]}
+        "data":  {"destinations": [OPJ(HOME, "smb", "data")]},
+        "nzb":   {"destinations": [OPJ(HOME, "nzbget")]},
+        "dotfiles":   {"destinations": [OPJ(HOME, "dotfiles")], 'alternatives': ['df']},
+        "smb":   {"destinations": [OPJ(HOME, "smb")]},
+        "misc":  {"destinations": [OPJ(HOME, "smb", "misc")]},
+        "audio": {"destinations": [OPJ(HOME, "smb", "audio")]},
+        "drive": {"destinations": [OPJ(HOME, "hd2", "gdrive"),
+                                   OPJ(HOME, "gdrive")]}
         }
 
 save(LOCS, JSON_FILE_PATH)
