a03e204ecb796e528f52e4179eaa5fe2cdaa8f46
Johan Kämpe <johan.kampe@chargestorm.se>
2018-12-18 22:36:36 +0100

    config: simplify
    Signed-off-by: Johan Kämpe <johan.kampe@chargestorm.se>

--- config.py
+++ config.py
@@ -6,34 +6,15 @@ import configparser
 import os
 import printing
 
-PRINT = printing.PrintClass(os.path.basename(__file__))
-
+DEFAULT_SETTINGS_FILE = 'settings.ini'
 
 class ConfigurationManager:
     def __init__(self, settings_file=None):
-        if not settings_file:
-            path_of_config_script = os.path.dirname(os.path.abspath(__file__))
-            self.filename = os.path.join(path_of_config_script, "settings.ini")
-        else:
-            self.filename = settings_file
-        self.config = None
-        self.load_success = False
-        self._load_settings()
+        settings = DEFAULT_SETTINGS_FILE
+        if settings_file:
+            settings = settings_file
+        self.config = configparser.ConfigParser()
+        self.config.read(settings)
 
-    def _load_settings(self):
-        if os.path.isfile(self.filename):
-            try:
-                self.config = configparser.ConfigParser()
-                self.config.read(self.filename)
-                self.load_success = True
-            except:
-                PRINT.error("could not load {}".format(self.filename))
-        else:
-            PRINT.error("file missing: {}".format(self.filename))
-
-    def get_setting(self, section, key):
-        if section in self.config and key in self.config[section]:
-            return self.config[section][key]
-        PRINT.error("{}:{} does not exist in {}"
-                    .format(section, key, self.filename))
-        return None
+    def get(self, key):
+        return self.config['default'][key]
