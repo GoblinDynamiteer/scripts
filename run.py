00e2bcf696b95bde0914d6d75a61869ac12c969d
Johan Kämpe <johan.kampe@chargestorm.se>
2018-12-18 19:38:42 +0100

    run: add method for determining if command exists
    on the system
    
    Signed-off-by: Johan Kämpe <johan.kampe@chargestorm.se>

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import printing
+import os
 
 CSTR = printing.to_color_str
 
@@ -18,3 +19,24 @@ def local_command(command, hide_output: bool = True, print_info: bool = True) ->
     if ret.returncode:
         print(f"{CSTR('command failed with return code: {ret.returncode}', 'red')}")
     return ret.returncode == 0
+
+
+def _executable(file_path):
+    return os.path.isfile(file_path) and os.access(file_path, os.X_OK)
+
+
+def program_exists(program):
+    "Determine if a command/program is available"
+    return command_exists(program)
+
+
+def command_exists(command):
+    "Determine if a command/program is available"
+    fpath, _ = os.path.split(command)
+    if fpath and _executable(command):
+        return True
+    for path in os.environ["PATH"].split(os.pathsep):
+        exe_file = os.path.join(path, command)
+        if _executable(exe_file):
+            return True
+    return False
