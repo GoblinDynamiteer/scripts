2c33717dfedb5a98591f9ad7c6917ccab17c3113
Johan Kämpe <kampe.johan@gmail.com>
2018-12-25 19:25:13 +0100

    dbmov_new: have _to_text be a function
    as pylint suggested..
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- dbmov_new.py
+++ dbmov_new.py
@@ -15,6 +15,12 @@ MOVIE_DATABASE_PATH = CFG.get('path_movdb_new')
 CSTR = printing.to_color_str
 
 
+def _to_text(movie_folder, movie_data):
+    scanned_hr = datetime.fromtimestamp(
+        movie_data['scanned']).strftime('%Y-%m-%d')
+    return f'[{scanned_hr}] [{movie_data["year"]}] [{movie_data["title"]}] [{movie_folder}]\n'
+
+
 class MovieDatabase(db_json.JSONDatabase):
     ''' Movie Database '''
 
@@ -39,15 +45,10 @@ class MovieDatabase(db_json.JSONDatabase):
                 return last_added_dict
         return last_added_dict
 
-    def _to_text(self, movie_folder, movie_data):
-        scanned_hr = datetime.fromtimestamp(
-            movie_data['scanned']).strftime('%Y-%m-%d')
-        return f'[{scanned_hr}] [{movie_data["year"]}] [{movie_data["title"]}] [{movie_folder}]\n'
-
     def export_last_added(self, target=os.path.join(CFG.get('path_film'), 'latest.txt')):
-        ''' Exports a list of the last added movies '''
+        ''' Exports the latest added movies to text file '''
         last_added = self.last_added(num=100)
-        last_added_text = [self._to_text(m, last_added[m]) for m in last_added]
+        last_added_text = [_to_text(m, last_added[m]) for m in last_added]
         try:
             with open(target, 'w') as last_added_file:
                 last_added_file.writelines(last_added_text)
