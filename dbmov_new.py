733437b7934e6cad626c93ce6235eaf65cd264ba
Johan Kämpe <kampe.johan@gmail.com>
2018-12-25 18:46:44 +0100

    dbmov_new: add last_added method
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- dbmov_new.py
+++ dbmov_new.py
@@ -22,3 +22,15 @@ class MovieDatabase(db_json.JSONDatabase):
         self.set_key_type('year', int)
         self.set_key_type('imdb', str)
         self.set_key_type('scanned', int)  # unix timestamp
+
+    def last_added(self, num=10):
+        ''' Get the most recently added movies '''
+        sorted_dict = self.sorted('scanned', reversed_sort=True)
+        count = 0
+        last_added_dict = {}
+        for folder, data in sorted_dict.items():
+            last_added_dict[folder] = data
+            count += 1
+            if count == num:
+                return last_added_dict
+        return last_added_dict
