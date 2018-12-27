a14e8ce227e0afd5b9991fbde3b6f71d7af2dae8
Johan Kämpe <kampe.johan@gmail.com>
2018-12-27 11:55:50 +0100

    util: add methods parse_imdbid and is_imdbid
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- util.py
+++ util.py
@@ -3,6 +3,7 @@
 ''' Various helper/utility functions '''
 
 import os
+import re
 
 
 def home_dir():
@@ -16,3 +17,18 @@ def dirname_of_file(file_path):
         return os.path.dirname(full_path)
     except:
         return None
+
+
+def parse_imdbid(string):
+    ''' Parse the IMDB-id from a string, if possible '''
+    match = re.search(r'tt\d{1,}', string)
+    if match:
+        return match.group()
+    return None
+
+
+def is_imdbid(string):
+    ''' Return true if string contains an IMDB-id '''
+    if parse_imdbid(string):
+        return True
+    return False
