75f597a59d13ececbeaebd86f9ec123a692b5dcb
Johan Kämpe <kampe.johan@gmail.com>
2018-12-27 12:13:26 +0100

    omdb: clean up script
    dont use a class
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- omdb.py
+++ omdb.py
@@ -1,94 +1,39 @@
 #!/usr/bin/env python3.6
 
+''' Searches the OMDb database '''
+
 import json
-import re
-import os
 import urllib.parse
 import urllib.request
 
+import util
+from config import ConfigurationManager
+
+CFG = ConfigurationManager()
+
 URL = "http://www.omdbapi.com"
+API_KEY = CFG.get('omdb_api_key')
 
 
-class omdb_search:
-    def __init__(self, query,
-                 type=None, year=None, api_key=None,
-                 season=None, episode=None):
-        _apk = self._load_api_key()
-        self.url_args = {}
-        if not _apk and api_key:
-            print("Using passed API-key")
-            _apk = api_key
-        elif not _apk:
-            print("no valid API-key found or passed, quitting")
-            quit()
-        self.url_args['apikey'] = _apk
-        self.json_data = ""
-        if self._is_imdb(query):
-            self.url_args['i'] = query
-        else:
-            self.url_args['t'] = query
-            if self._valid_year(year):
-                self.url_args['y'] = year
-            if self._valid_type(type):
-                self.url_args['type'] = type
-        self.url_args['plot'] = "full"
-        if season:
-            self.url_args['Season'] = season
-        if episode:
-            self.url_args['Episode'] = episode
-        self.search_string_url = URL + "?" + \
-            urllib.parse.urlencode(self.url_args)
-        self._search()
+def _omdb_search(url):
+    json_response = {}
+    try:
+        response = urllib.request.urlopen(
+            url, timeout=4).read().decode("utf-8")
+        json_response = json.loads(response)
+    except:
+        pass
+    return json_response
 
-    def get_url(self):
-        ''' Returns generated URL used for query '''
-        return self.search_string_url
 
-    def _search(self):
-        try:
-            response = urllib.request.urlopen(
-                self.search_string_url, timeout=4).read().decode("utf-8")
-            self.json_data = json.loads(response)
-        except:
-            self.json_data = None
-
-    # Load API-key from text-file in same directory as script file
-    def _load_api_key(self):
-        script_path = os.path.dirname(os.path.realpath(__file__))
-        api_txt = os.path.join(script_path, "omdb_api.txt")
-        try:
-            f = open(api_txt, "r")
-            key = f.readline().strip('\n')
-            f.close()
-            return key
-        except:
-            print("Could not load OMDb API-key from file")
-            return None
-
-    def data(self):
-        return self.json_data
-
-    def get_type(self):
-        try:
-            return self.json_data["Type"]
-        except:
-            return None
-
-    @staticmethod
-    def _is_imdb(string):
-        re_imdb = re.compile(r"^tt\d{1,}")
-        return True if re_imdb.search(string) else False
-
-    @staticmethod
-    def _valid_type(string):
-        if string is None:
-            return False
-        re_type = re.compile(r"(^movie$|^series$|^episode$)")
-        return True if re_type.search(string) else False
-
-    @staticmethod
-    def _valid_year(string):
-        if string is None:
-            return False
-        re_year = re.compile(r"^[1-2]\d{3}$")
-        return True if (re_year.search(string) or string != None) else False
+def movie_search(query_string, year=None):
+    url_args = {'apikey': API_KEY, 'type': 'movie'}
+    if util.is_imdbid(query_string):
+        url_args['i'] = util.parse_imdbid(query_string)
+    else:
+        url_args['t'] = query_string
+    if util.is_valid_year(year):
+        url_args['y'] = year
+    url = f'{URL}?{urllib.parse.urlencode(url_args)}'
+    print(url)
+    return _omdb_search(url)
