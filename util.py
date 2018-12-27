87b03dbdd3a1c8b553e88019b7f727027a463600
Johan Kämpe <kampe.johan@gmail.com>
2018-12-27 10:41:28 +0100

    util: add dirname_of_file method
    tries to return the path to a the dir containing
    the passed file.
    
    example:
    /home/adam/scripts/run.py
    would return
    /home/adam/scripts
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.6
 
-''' TV Tools '''
+''' Various helper/utility functions '''
 
 import os
 
@@ -8,3 +8,11 @@ import os
 def home_dir():
     ''' Full path to home directory '''
     return os.path.expanduser("~")
+
+
+def dirname_of_file(file_path):
+    try:
+        full_path = os.path.realpath(file_path)
+        return os.path.dirname(full_path)
+    except:
+        return None
