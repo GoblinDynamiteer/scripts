ea56fb8985018fd9bfe133d2580ca4dfec32a064
Johan Kämpe <kampe.johan@gmail.com>
2019-02-23 10:59:40 +0100

    diskstation: remve restore share
    no need for it anymore
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- diskstation.py
+++ diskstation.py
@@ -21,18 +21,12 @@ def is_ds_special_dir(dir_str):
 
 def get_ds_shares() -> list:
     " Get a list of available DS shares "
-    return ['APPS', 'TV', 'FILM', 'MISC', 'BACKUP', 'DATA', 'DOCEDU', 'AUDIO', 'Rest']
+    return ['APPS', 'TV', 'FILM', 'MISC', 'BACKUP', 'DATA', 'DOCEDU', 'AUDIO']
 
 
 def get_mount_dest() -> str:
     " Get local mount destination path "
     return CONFIG.get("ds_mount_path")
-
-
-def _get_credentials_file(share):
-    if share == 'Rest':
-        return CONFIG.get('smb_credentials_drb')
-    return CONFIG.get('smb_credentials_reg')
 
 
 def _print_error_invalid_share(string):
@@ -46,6 +40,7 @@ def mount(ds_share):
         sys.exit(1)
     mount_dest = get_mount_dest()
     ds_shares = get_ds_shares()
+    cred_file = CONFIG.get('smb_credentials_reg')
     if ds_share == "all" or ds_share.upper() in ds_shares:
         for share in ds_shares:
             if share == ds_share.upper() or ds_share == "all":
@@ -53,7 +48,6 @@ def mount(ds_share):
                     PRINT.info(
                         f"{share} is mounted at {get_mount_path(share)}")
                     continue
-                cred_file = _get_credentials_file(share)
                 opt = f"credentials={cred_file},iocharset=utf8,vers=3.0,rw,file_mode=0777,dir_mode=0777"
                 src = f"//{NAS_IP}/{share}"
                 local_dest = f"{mount_dest}{share.lower()}"
@@ -68,8 +62,6 @@ def mount(ds_share):
 
 def ismounted(ds_share) -> bool:
     " Check if share is mounted"
-    if ds_share.lower() == 'rest':
-        return False
     ds_shares = get_ds_shares()
     mount_dest = get_mount_dest()
     if ds_share.upper() in ds_shares:
@@ -88,8 +80,6 @@ def ismounted(ds_share) -> bool:
 
 def get_mount_path(ds_share):
     " Gets mount full mount path on DS "
-    if ds_share.lower() == 'rest':
-        return None
     ds_shares = get_ds_shares()
     mount_dest = get_mount_dest()
     if ds_share.upper() in ds_shares:
@@ -104,8 +94,6 @@ def print_ifmounted(ds_share) -> None:
     " Prints out information about share mount status "
     ds_shares = get_ds_shares()
     for share in ds_shares:
-        if share == 'Rest':
-            continue
         if ds_share == "all" or ds_share.lower() == share:
             if ismounted(share):
                 PRINT.info(f"{share} is mounted at {get_mount_path(share)}")
