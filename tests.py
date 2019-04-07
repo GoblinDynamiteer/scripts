2e6e5f91fa87939d2fdbcd3c849ea93d665faf7a
Johan Kämpe <kampe.johan@gmail.com>
2019-04-07 19:05:03 +0200

    tests: add util_tv and util_movie tests
    and remove obsolete filetools tests
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- tests.py
+++ tests.py
@@ -3,27 +3,52 @@
 ''' Subtitle tools '''
 
 import unittest
-import filetools
+import util_movie
+import util_tv
 import printing
 
 
-class TestFileTools(unittest.TestCase):
+class TestUtilMovie(unittest.TestCase):
 
-    def test_fix_invalid_folder_or_file_name(self):
-        self.assertEqual(
-            filetools.fix_invalid_folder_or_file_name('A B'), 'A.B')
-        self.assertEqual(
-            filetools.fix_invalid_folder_or_file_name('Name.blu-ray.- SceNE'), 'Name.BluRay-SceNE')
-        self.assertEqual(
-            filetools.fix_invalid_folder_or_file_name('name-'), 'name')
+    def test_is_movie(self):
+        m = util_movie.is_movie
+        self.assertEqual(m(
+            'TvShow.S02.720p.HDTV.x264-SceneName'), False)
+        self.assertEqual(m(
+            'MovieName.2014.720p.BluRay.x264-aFP'), True)
+        self.assertEqual(m(
+            'MovieName.2160p.BluRay.x264-aFP'), True)
 
-    def test_guess_folder_type(self):
-        self.assertEqual(filetools.guess_folder_type(
-            'TvShow.S02.720p.HDTV.x264-SceneName'), 'season')
-        self.assertEqual(filetools.guess_folder_type(
-            'MovieName.2014.720p.BluRay.x264-aFP'), 'movie')
-        self.assertEqual(filetools.guess_folder_type(
-            'MovieName.2160p.BluRay.x264-aFP'), 'movie')
+
+class TestUtilTv(unittest.TestCase):
+
+    def test_season_episode_parser(self):
+        m = util_tv.parse_season_episode
+        self.assertEqual(m('TvShow.S02.720p.HDTV.x264-SceneName'), (2, None))
+        self.assertEqual(m('TvShow.S32E12.720p.HDTV.x264-SceneName'), (32, 12))
+        self.assertEqual(
+            m('TvShow.E12.720p.HDTV.x264-SceneName'), (None, None))
+
+    def test_parse_season(self):
+        m = util_tv.parse_season
+        self.assertEqual(m('TvShow.S02.720p.HDTV.x264-SceneName'), 2)
+        self.assertEqual(m('TvShow.S32E12.720p.HDTV.x264-SceneName'), 32)
+        self.assertEqual(
+            m('TvShow.E12.720p.HDTV.x264-SceneName'), None)
+
+    def test_is_episode(self):
+        m = util_tv.is_episode
+        self.assertEqual(m('TvShow.S02.720p.HDTV.x264-SceneName'), False)
+        self.assertEqual(m('TvShow.S32E12.720p.HDTV.x264-SceneName'), True)
+        self.assertEqual(
+            m('TvShow.E12.720p.HDTV.x264-SceneName'), False)
+
+    def test_is_season(self):
+        m = util_tv.is_season
+        self.assertEqual(m('TvShow.S02.720p.HDTV.x264-SceneName'), True)
+        self.assertEqual(m('TvShow.S32E12.720p.HDTV.x264-SceneName'), False)
+        self.assertEqual(
+            m('TvShow.E12.720p.HDTV.x264-SceneName'), False)
 
 
 class TestStrOut(unittest.TestCase):
