0c6acbde0caabc128903dcef3146273dda916d87
Johan Kämpe <kampe.johan@gmail.com>
2019-04-13 12:21:33 +0200

    goto: add support for subfolder matching
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- goto.py
+++ goto.py
@@ -5,6 +5,7 @@
 import os
 import sys
 import json
+import util
 
 
 def load(json_file):
@@ -45,28 +46,48 @@ class Goto(object):
         return None
 
 
-HOME = os.path.expanduser("~")
-SCRIPT_DIR = os.path.realpath(__file__)
-JSON_FILE_PATH = os.path.join(os.path.dirname(SCRIPT_DIR), "goto_locs.json")
-LOCS = load(JSON_FILE_PATH)
+def _match_subdir(orig_path, sub_str: str):
+    if not util.is_dir(orig_path):
+        return ""
+    for item in os.listdir(orig_path):
+        if util.is_file(os.path.join(orig_path, item)):
+            continue
+        if sub_str.lower() in item.lower():
+            return item  # TODO: handle several matches
+    return ""
 
-GOTOS = []
-ARGUMENT = None
 
-for sc in LOCS:
-    g = Goto(sc)
-    for d in LOCS[sc]["destinations"]:
-        g.add_destination(d)
-    try:
-        for a in LOCS[sc]["alternatives"]:
-            g.add_shortcut(a)
-    except KeyError:
-        pass
-    GOTOS.append(g)
+if __name__ == '__main__':
+    HOME = os.path.expanduser("~")
+    SCRIPT_DIR = os.path.realpath(__file__)
+    JSON_FILE_PATH = os.path.join(
+        os.path.dirname(SCRIPT_DIR), "goto_locs.json")
+    LOCS = load(JSON_FILE_PATH)
 
-if len(sys.argv) > 1:
-    ARGUMENT = sys.argv[1]
-    for goto in GOTOS:
-        for sc in goto.get_shortcuts():
-            if ARGUMENT == sc:
-                print(goto.get_path())
+    GOTOS = []
+    ARGUMENT = None
+
+    for sc in LOCS:
+        g = Goto(sc)
+        for d in LOCS[sc]["destinations"]:
+            g.add_destination(d)
+        try:
+            for a in LOCS[sc]["alternatives"]:
+                g.add_shortcut(a)
+        except KeyError:
+            pass
+        GOTOS.append(g)
+
+    if len(sys.argv) > 1:
+        ARGUMENT = sys.argv[1]
+        for goto in GOTOS:
+            for sc in goto.get_shortcuts():
+                if ARGUMENT == sc:
+                    path = goto.get_path()
+                    if len(sys.argv) > 2:
+                        for index, arg in enumerate(sys.argv):
+                            if index < 2:
+                                continue
+                            subdir = _match_subdir(path, sys.argv[index])
+                            path = os.path.join(path, subdir)
+                    print(path)
