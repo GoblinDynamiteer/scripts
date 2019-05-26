20f2e325b942dc11085f43dfebb407d0b11d1261
Johan Kämpe <johan.kampe@chargestorm.se>
2019-05-26 16:37:28 +0200

    diskstation: use new printing script
    Signed-off-by: Johan Kämpe <johan.kampe@chargestorm.se>

--- diskstation.py
+++ diskstation.py
@@ -7,10 +7,9 @@ import os
 import sys
 import platform
 import config
-import printing
+from printing import cstr, pcstr
 from run import local_command
 
-PRINT = printing.PrintClass(os.path.basename(__file__))
 CONFIG = config.ConfigurationManager()
 NAS_IP = CONFIG.get('ds_ip')
 
@@ -30,13 +29,13 @@ def get_mount_dest() -> str:
 
 
 def _print_error_invalid_share(string):
-    PRINT.error(f"invalid share: [{string}]")
+    print(f"invalid share: {cstr(string, 'red')}")
 
 
 def mount(ds_share):
     " Mount a DS share on a linux system "
     if platform.system() != 'Linux':
-        PRINT.error("mount: Not on a Linux-system, quitting.")
+        pcstr("not on a linux-system, quitting", "red")
         sys.exit(1)
     mount_dest = get_mount_dest()
     ds_shares = get_ds_shares()
@@ -45,17 +44,16 @@ def mount(ds_share):
         for share in ds_shares:
             if share == ds_share.upper() or ds_share == "all":
                 if ismounted(share):
-                    PRINT.info(
-                        f"{share} is mounted at {get_mount_path(share)}")
+                    print(f"{cstr(share, 'lgreen')} is mounted at {get_mount_path(share)}")
                     continue
                 opt = f"credentials={cred_file},iocharset=utf8,vers=3.0,rw,file_mode=0777,dir_mode=0777"
                 src = f"//{NAS_IP}/{share}"
                 local_dest = f"{mount_dest}{share.lower()}"
-                PRINT.info(f"mounting {share} to {local_dest}")
+                print(f"mounting {share} to {local_dest}")
                 success = local_command(
                     f"sudo mount -t cifs {src} {local_dest} -o {opt}", print_info=False)
                 if not success:
-                    PRINT.warning(f"mounting of {src} failed!")
+                    print(f"mounting of {cstr(src, 'red')} failed!")
     else:
         _print_error_invalid_share(ds_share)
 
@@ -67,7 +65,7 @@ def ismounted(ds_share) -> bool:
     if ds_share.upper() in ds_shares:
         local_dest = os.path.join(mount_dest, ds_share.lower())
         if not os.path.exists(local_dest):
-            PRINT.info(f"creating mount destination [{local_dest}]")
+            print(f"creating mount destination {cstr(local_dest, 'green')}")
             os.makedirs(local_dest)
         subdirs = os.listdir(local_dest)
         if subdirs:
@@ -96,9 +94,9 @@ def print_ifmounted(ds_share) -> None:
     for share in ds_shares:
         if ds_share == "all" or ds_share.lower() == share:
             if ismounted(share):
-                PRINT.info(f"{share} is mounted at {get_mount_path(share)}")
+                print(f"{share} is mounted at {get_mount_path(share)}")
             else:
-                PRINT.info(f"{share} is not mounted!")
+                print(f"{share} is not mounted!")
 
 
 if __name__ == "__main__":
