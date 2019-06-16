4bd0c3f1c0aabbfded78a165c0039d2eb3b95614
Johan Kämpe <kampe.johan@gmail.com>
2019-06-16 10:53:31 +0200

    db_json: add debug variable and use new cstr
    debug shall be used to supress/enable debug print
    messages
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- db_json.py
+++ db_json.py
@@ -6,24 +6,24 @@ import json
 import os
 import shutil
 
-import printing
+from printing import cstr
 import util
 from config import ConfigurationManager
 
-CSTR = printing.to_color_str
 CFG = ConfigurationManager()
 
 
 class JSONDatabase(object):
     ''' A simple json database '''
 
-    def __init__(self, database_file_path):
+    def __init__(self, database_file_path, debug_print: bool = False):
         self.json = None
         self.db_file_path = database_file_path
         self._load_database_file()
         self.valid_keys = []
         self.primary_key = None
         self.key_types = {}
+        self.debug = debug_print
 
     def _load_database_file(self):
         try:
@@ -40,10 +40,13 @@ class JSONDatabase(object):
         try:
             with open(self.db_file_path, 'w') as database_file:
                 json.dump(self.json, database_file)
+            if self.debug:
+                print(f'saved database file {cstr(self.db_file_path, "lblue")}')
         except:
-            print('could not save database')
+            if self.debug:
+                print('could not save database')
 
-    def _backup(self, debug_print=False):
+    def _backup(self):
         ''' Backup json file '''
         timestamp = util.now_timestamp()
         backup_path = CFG.get('path_backup')
@@ -54,8 +57,8 @@ class JSONDatabase(object):
             shutil.copy(self.db_file_path, destination)
         except PermissionError:
             shutil.copyfile(self.db_file_path, destination)
-        if debug_print:
-            print(f'backed up to {CSTR(destination, "green")}')
+        if self.debug:
+            print(f'backed up to {cstr(destination, "green")}')
 
     def save(self):
         ''' Save database file '''
@@ -72,10 +75,10 @@ class JSONDatabase(object):
     def set_key_type(self, key, data_type):
         ''' Constrict key data type '''
         if key not in self.valid_keys:
-            print(f'{CSTR(key, "red")} is not a valid key!')
+            print(f'{cstr(key, "red")} is not a valid key!')
             return False
         if data_type not in [int, str, float, list, dict, bool]:
-            print(f'{CSTR(str(data_type), "red")} is not a valid data type!')
+            print(f'{cstr(str(data_type), "red")} is not a valid data type!')
             return False
         self.key_types[key] = data_type
 
@@ -83,15 +86,15 @@ class JSONDatabase(object):
         ''' Updates data for an entry '''
         if primary_key not in self.json:
             print(
-                f'can\'t update {CSTR(f"{primary_key}", "orange")} not in db')
+                f'can\'t update {cstr(f"{primary_key}", "orange")} not in db')
             return False
         if data not in self.valid_keys:
-            print(f'{CSTR(data, "red")} is not a valid key!')
+            print(f'{cstr(data, "red")} is not a valid key!')
             return False
         if self.key_types.get(data, None):
             if not isinstance(value, self.key_types[data]):
                 print(
-                    f'{CSTR(data, "red")} is not of data type {str(self.key_types[data])}!')
+                    f'{cstr(data, "red")} is not of data type {str(self.key_types[data])}!')
                 return False
         self.json[primary_key][data] = value
         return True
@@ -116,7 +119,7 @@ class JSONDatabase(object):
             return self.json[primary_key][data]
         except KeyError:
             print(
-                f'could not retrieve data [{data}] for {CSTR(primary_key, "red")}')
+                f'could not retrieve data [{data}] for {cstr(primary_key, "red")}')
             return None
 
     def exists(self, primary_key):
@@ -162,23 +165,23 @@ class JSONDatabase(object):
         keys = list(data.keys())
         if self.primary_key not in keys:
             print(
-                f'missing primary key: {CSTR(f"{self.primary_key}", "orange")}')
+                f'missing primary key: {cstr(f"{self.primary_key}", "orange")}')
             return False
         invalid_keys = [k for k in keys if k not in self.valid_keys]
         if invalid_keys:
             print(
-                f'invalid key(s) for database: {CSTR(f"{invalid_keys}", "red")}')
+                f'invalid key(s) for database: {cstr(f"{invalid_keys}", "red")}')
             return False
         for key in keys:
             if self.key_types.get(key, None):
                 if not isinstance(data[key], self.key_types[key]):
                     print(
-                        f'{CSTR(key, "red")} is not of',
+                        f'{cstr(key, "red")} is not of',
                         f'data type {str(self.key_types[key])}!')
                     return False
         primary = data[self.primary_key]
         if primary in self.json:
-            print(f'{CSTR(primary, "red")} already in database, use update instead!')
+            print(f'{cstr(primary, "red")} already in database, use update instead!')
             return False
         data.pop(self.primary_key)
         self.json[primary] = data
