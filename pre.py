7c3bd81c9a3a43193c4389946f4f74b41c91f791
Johan Kämpe <kampe.johan@gmail.com>
2019-07-15 13:02:08 +0200

    pre: use a replacement list instead of trimming filename query
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- pre.py
+++ pre.py
@@ -8,68 +8,49 @@ import json
 import sys
 from pathlib import Path
 
-import time
-
 import requests
+
+import config
+
+
+CFG = config.ConfigurationManager()
+
+
+def run_replace_list_on_query(query_string):
+    replace_file_path = Path(CFG.path("scripts")) / 'pre_replace.json'
+    with open(replace_file_path) as replace_fp:
+        for string, replacement in json.load(replace_fp).items():
+            query_string = query_string.replace(string, replacement)
+    return query_string
 
 
 def pre_search(query: str) -> list:
     """ runs a pre search, returns list matched names """
+    print("got q", query)
     json_response = requests.get(
         f"https://predb.ovh/api/v1/?q={query}&count=50")
     try:
         data = json.loads(json_response.text)
         rows = data['data']['rows']
     except json.decoder.JSONDecodeError:
-        print("got json decoder error!")
+        print(f"got json decoder error! on query: {query}")
+        print(json_response)
         return False
     return [row['name'] for row in rows]
 
 
-def _query_list_trim(queries: list, left: int = 0, right: int = 0):
-    list_len = len(queries)
-    if not right:
-        right = list_len
-    if left > list_len or abs(right) > list_len:
-        return []
-    if left > abs(right):
-        return []
-    return queries[left:right]
-
-
-def pre_search_from_file(file_name: str) -> str:
+def pre_search_from_file(file_name: str, use_replacement_list=True) -> str:
     """ runs a pre search, return best match if possible """
+    file_name = run_replace_list_on_query(
+        file_name) if use_replacement_list else file_name
     split_filename = file_name.split('.')
-    result = []
-    trim_right = 0
-    trim_left = 0
-    query_lists = {"left": split_filename,
-                   "right": split_filename, "both": split_filename}
-    while True:
-        query_lists["left"] = _query_list_trim(split_filename, left=trim_left)
-        query_lists["right"] = _query_list_trim(
-            split_filename, right=trim_right)
-        query_lists["both"] = _query_list_trim(
-            split_filename, left=trim_left, right=trim_right)
-
-        if not query_lists["right"] and not query_lists["left"]:
-            break
-
-        for _, query_list in query_lists.items():
-            if not query_list:
-                continue
-            query = f'"{" ".join(query_list)}"'
-            if not query:
-                continue
-            result = pre_search(query)
-            if result:
-                for res in result:
-                    if '1080p' in res:  # prefer 1080p results
-                        return res
-                return result[0]
-            trim_right -= 1
-            trim_left += 1
-            time.sleep(2)
+    query = f'{" ".join(split_filename)}'
+    results = pre_search(query)
+    if results:
+        for result in results:
+            if '1080p' in result:  # prefer 1080p results
+                return result
+        return results[0]
     return ""
 
 
