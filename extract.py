e3df408cbe19a3bb5555bef5f7833af6e6437cd2
Johan Kämpe <kampe.johan@gmail.com>
2019-12-14 11:21:40 +0100

    extract: rewrite to use pathlib class
    and add alot of validation and error checking
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- extract.py
+++ extract.py
@@ -19,72 +19,85 @@ OPJ = os.path.join
 CFG = config.ConfigurationManager()
 
 
-def _find_rar(source_dir):
-    rar_files = [f for f in os.listdir(
-        source_dir) if f.endswith('.rar')]
+def validate_path(path):
+    if isinstance(path, str):
+        path = Path(path)
+    if not isinstance(path, Path):
+        print(f"invalid path: {path}")
+        return None
+    path = path.resolve()
+    if not path.exists():
+        print(f"path does not exist: {str(path)}")
+        return None
+    return path
+
+
+def find_rar_in_path(path):
+    path = validate_path(path)
+    if not path:
+        return None
+    rar_files = list(path.glob("**/*.rar"))
     if len(rar_files) > 1:
         for rar_file in rar_files:
-            if '01.rar' in rar_file:
-                rar_files = [rar_file]
-                break
-    if not rar_files or len(rar_files) > 1:
+            if rar_file.name.endswith("01.rar"):
+                return rar_file
+    try:
+        return rar_files[0]
+    except IndexError:
         return None
-    full_path = OPJ(os.getcwd(), source_dir, rar_files[0])
-    if util.is_file(full_path):
-        return full_path
-    return None
 
 
-def _find_mkv(source_dir):
-    mkv_files = [f for f in os.listdir(
-        source_dir) if f.endswith('.mkv') and "sample" not in f.lower()]
-    if not mkv_files or len(mkv_files) > 1:
+def find_mkv_in_path(path):
+    path = validate_path(path)
+    if not path:
         return None
-    full_path = OPJ(os.getcwd(), source_dir, mkv_files[0])
-    if util.is_file(full_path):
-        return full_path
-    return None
-
-
-def _find_nfo(source_dir):
-    nfo_files = [f for f in os.listdir(
-        source_dir) if f.endswith('.nfo')]
-    if not nfo_files:
+    mkv_files = [f for f in path.glob("**/*.mkv") if "sample" not in f.name]
+    if len(mkv_files) > 1:
+        return None  # TODO: determine which file is valid
+    try:
+        return mkv_files[0]
+    except IndexError:
         return None
-    full_path = OPJ(os.getcwd(), ARGS.source, nfo_files[0])
-    if util.is_file(full_path):
-        return full_path
-    return None
 
 
-def _movie_dest(source_dir):
+def find_nfo_file_in_path(path):
+    path = validate_path(path)
+    if not path:
+        return None
+    try:
+        return list(path.glob("**/*.nfo"))[0]  # TODO: validate nfo file?
+    except IndexError:
+        return None
+
+
+def determine_movie_destination(source_dir):
     letter = util_movie.determine_letter(source_dir)
     return OPJ(CFG.get('path_film'), letter, source_dir)
 
 
-def _episode_dest(source_dir):
-    show = util_tv.determine_show_from_episode_name(source_dir)
+def determine_episode_destination(episode_name):
+    show = util_tv.determine_show_from_episode_name(episode_name)
     if not show:
-        print(f"could not determine show for {cstr(source_dir, 'lblue')}")
-        show = util_tv.guess_show_name_from_episode_name(source_dir)
+        print(f"could not determine show for {cstr(episode_name, 'lblue')}")
+        show = util_tv.guess_show_name_from_episode_name(episode_name)
         if not show:
             return None
         print(f"guessing: {cstr(show, 'orange')}")
     path = OPJ(CFG.get('path_tv'), show)
     if not os.path.exists(path):
         print(f"{cstr(path, 'orange')} does not exist! will create")
-    season = util_tv.parse_season(source_dir)
+    season = util_tv.parse_season(episode_name)
     if not season:
-        print(f"could not determine season of {cstr(source_dir, 'orange')}")
+        print(f"could not determine season of {cstr(episode_name, 'orange')}")
         return None
     return OPJ(path, f'S{season:02d}')
 
 
 def process_movie_dir(movie_dir_source):
     pfcs(f"processing: i[{movie_dir_source}] as type b[movie dir]")
-    nfo_loc = _find_nfo(movie_dir_source)
-    rar_loc = _find_rar(movie_dir_source)
-    mkv_loc = _find_mkv(movie_dir_source)
+    nfo_loc = find_nfo_file_in_path(movie_dir_source)
+    rar_loc = find_rar_in_path(movie_dir_source)
+    mkv_loc = find_mkv_in_path(movie_dir_source)
     if not rar_loc and not mkv_loc:
         pfcs(f"could e[not] find item to process in w[{movie_dir_source}]!")
         return
@@ -92,7 +105,7 @@ def process_movie_dir(movie_dir_source):
         pfcs(f"found e[both] rar and mkv in w[{movie_dir_source}]!")
         return
     pfcs(f"found file: i[{mkv_loc or rar_loc}]")
-    dest = _movie_dest(movie_dir_source)
+    dest = determine_movie_destination(movie_dir_source)
     pfcs(f"destination: i[{dest}]")
     if rar_loc:
         if not run.extract(rar_loc, dest, create_dirs=True):
@@ -109,66 +122,77 @@ def process_movie_dir(movie_dir_source):
     print(f'removed {cstr(movie_dir_source, "orange")}')
 
 
-def process_movie_file(movie_file_source):
-    pfcs(f"processing: i[{movie_file_source}] as type b[movie file]")
-    directory = ""
-    for ext in util.video_extensions():
-        if movie_file_source.endswith(ext):
-            directory = movie_file_source.replace(ext, "")
-            break
-    if not directory:
-        pfcs(f"could not determine destination for w[{movie_file_source}]")
+def process_movie_file(movie_file_path):
+    movie_path = validate_path(movie_file_path)
+    if not movie_file_path:
         return
-    dest = _movie_dest(directory)
+    if not movie_file_path.is_file():
+        print(f"path {movie_file_path.name} is not a file!")
+        return
+    pfcs(f"processing: i[{movie_file_path}] as type b[movie file]")
+    if not movie_file_path.suffix in util.video_extensions():
+        pfcs(f"could not determine destination for w[{movie_file_path.name}]")
+        return
+    directory = str(movie_file_path).replace(movie_file_path.suffix, "")
+    dest = determine_movie_destination(directory)
     pfcs(f"destination: i[{dest}]")
-    run.move_file(movie_file_source, dest, create_dirs=True)
+    run.move_file(movie_file_path, dest, create_dirs=True)
 
 
-def process_movie(movie_source):
-    if util.is_dir(movie_source):
-        process_movie_dir(movie_source)
+def process_movie(movie_path: Path):
+    movie_path = validate_path(movie_path)
+    if not movie_path:
+        return
+    if movie_path.is_dir():
+        process_movie_dir(movie_path)
     else:
-        process_movie_file(movie_source)
+        process_movie_file(movie_path)
 
 
-def process_episode(episode_source):
-    if util.is_dir(episode_source):
-        pfcs(f"processing: i[{episode_source}] as type b[episode dir]")
-        rar_loc = _find_rar(episode_source)
-        dest = _episode_dest(episode_source)
-        if not dest:
-            pfcs(f"could not determine destination for w[{episode_source}]")
-            return
+def process_episode(ep_path: Path):
+    ep_path = validate_path(ep_path)
+    if not ep_path:
+        return
+    dest = determine_episode_destination(ep_path.name)
+    if not dest:
+        pfcs(f"could not determine destination for w[{ep_path}]")
+        return
+    if ep_path.is_dir():
+        pfcs(f"processing: i[{ep_path.name}] as type b[episode dir]")
+        rar_loc = find_rar_in_path(ep_path)
         if not run.extract(rar_loc, dest, create_dirs=True):
             return  # extract failed
-    else:
-        pfcs(f"processing: i[{episode_source}] as type b[episode file]")
-        run.move_file(episode_source, _episode_dest(
-            episode_source), create_dirs=True)
+        return
+    pfcs(f"processing: i[{ep_path.name}] as type b[episode file]")
+    run.move_file(ep_path, dest, create_dirs=True)
 
 
-def _handle_item(source_item):
-    if util_movie.is_movie(source_item):
-        process_movie(source_item)
-    elif util_tv.is_episode(source_item):
-        process_episode(source_item)
-    elif util_tv.is_season(source_item):
-        if util.is_dir(source_item):
-            pfcs(f"processing: i[{source_item}] as type b[season dir]")
-            os.chdir(source_item)
-            for item in os.listdir('.'):
-                _handle_item(str(item))
-            pfcs(f"g[done!] please remove w[{source_item}] manually.")
+def _handle_item(source_item_path):
+    source_item_path = validate_path(source_item_path)
+    if not source_item_path:
+        return
+    name = source_item_path.name
+    if util_movie.is_movie(name):
+        process_movie(source_item_path)
+    elif util_tv.is_episode(name):
+        process_episode(source_item_path)
+    elif util_tv.is_season(name):
+        if source_item_path.is_dir():
+            pfcs(f"processing: i[{name}] as type b[season dir]")
+            for item in source_item_path.iterdir():
+                _handle_item(item)
+            pfcs(f"g[done!] please remove w[{name}] manually.")
     else:
-        pfcs(f"could not determine type of w[{source_item}]")
+        pfcs(f"could not determine type of w[{name}]")
 
 
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser(description='extractor')
     PARSER.add_argument('source', type=str, help='item(s) to process')
     ARGS, _ = PARSER.parse_known_args()
+    CURRENT_DIR = Path.cwd()
     if '*' in ARGS.source:
         items = glob.glob(ARGS.source)
-        [_handle_item(i) for i in items]
+        [_handle_item(CURRENT_DIR / i) for i in items]
     else:
-        _handle_item(ARGS.source)
+        _handle_item(Path(ARGS.source))
