cd8c6c956b20c5176be091d606cb914bb07b04fa
Johan Kämpe <kampe.johan@gmail.com>
2020-04-09 11:22:58 +0200

    img: add resizing method
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- img.py
+++ img.py
@@ -5,6 +5,7 @@
 from enum import IntEnum
 
 from printing import pfcs
+from pathlib import Path
 import util
 
 try:
@@ -20,7 +21,7 @@ class ImageOrientation(IntEnum):
     Square = 2
 
 
-def image_size(path_to_image: str)->tuple:
+def image_size(path_to_image: str) -> tuple:
     if not util.is_file(path_to_image):
         pfcs(f'image file e[{path_to_image}] does not exist!')
         return None
@@ -29,15 +30,41 @@ def image_size(path_to_image: str)->tuple:
     return None
 
 
-def image_heigh(path_to_image: str)->int:
+def image_heigh(path_to_image: str) -> int:
     return image_size(path_to_image)[1]
 
 
-def image_width(path_to_image: str)->int:
+def image_width(path_to_image: str) -> int:
     return image_size(path_to_image)[0]
 
 
-def image_orientation(path_to_image: str)-> ImageOrientation:
+def image_resize(path_to_image: str, width: int, heigh: int, new_file_name: None) -> bool:
+    if not util.is_file(path_to_image):
+        pfcs(f'image file e[{path_to_image}] does not exist!')
+        return False
+    old_img_path = Path(path_to_image)
+    if not new_file_name:
+        new_file_name = old_img_path.name.replace(
+            old_img_path.suffix, "") + "_resized" + old_img_path.suffix
+    elif isinstance(new_file_name, str):
+        resized_img_path = Path(old_img_path.parent) / new_file_name
+    elif isinstance(new_file_name, Path):
+        resized_img_path = new_file_name
+    else:
+        pfcs(f'cannot determine output filename of'
+             f'resized version of e[{path_to_image}]!')
+        return False
+    if util.is_file(resized_img_path):
+        pfcs(f'output filename e[{resized_img_path}] already exist!')
+        return False
+    with Image.open(old_img_path) as image:
+        resized_image = image.copy()
+        resized_image.thumbnail((width, heigh))
+        resized_image.save(resized_img_path)
+        return True
+
+
+def image_orientation(path_to_image: str) -> ImageOrientation:
     width, height = image_size(path_to_image)
     if width > height:
         return ImageOrientation.Landscape
