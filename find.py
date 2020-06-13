5b532628366fe8b3e515aaa595395f48bb91f889
Johan Kämpe <kampe.johan@gmail.com>
2020-06-13 14:13:16 +0200

    find: add script (WIP)
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

---
+++ find.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3.8
+
+from printing import fcs
+from run import local_command
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def gen_args():
+    parser = ArgumentParser("finder script")
+    parser.add_argument("--full-paths",
+                        "-f",
+                        action="store_true",
+                        dest="full_paths")
+    parser.add_argument("--highlight",
+                        "-c",
+                        dest="highlight",
+                        default="")
+    parser.add_argument("--first",
+                        action="store_true",
+                        help="show only first match")
+    parser.add_argument("--subprocess",
+                        "-s",
+                        default="",
+                        help="run command, use {} to insert filename")
+    return parser.parse_args()
+
+
+class FileInfo():
+    def __init__(self, file_path, args):
+        self.path = file_path
+        self.args = args
+        if args.highlight:
+            self.highlighted = str(file_path).replace(
+                args.highlight, fcs(f"b[{args.highlight}]"))
+        else:
+            self.highlighted = ""
+        self.filename = file_path.name
+        self.colorized_filename = fcs(f"i[{file_path.name}]")
+
+    def print(self):
+        if self.args.full_paths:
+            if self.args.highlight:
+                print(self.highlighted)
+            else:
+                print(str(self.path).replace(
+                    self.filename, self.colorized_filename))
+        else:
+            print(self.filename)
+
+    def subp(self, command):
+        command = command.replace("{}", self.filename)
+        print(command)
+
+
+def main():
+    args = gen_args()
+    current_path = Path.cwd()
+    files = current_path.glob("*.*")
+    for file_path in sorted(files):
+        file_info = FileInfo(file_path, args)
+        file_info.print()
+        if args.subprocess:
+            file_info.subp(args.subprocess)
+        if args.first:
+            break
+
+
+if __name__ == "__main__":
+    main()
