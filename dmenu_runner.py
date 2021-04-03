ff8d7450ffa6a44159fd91cbedcc84a96f9b9895
JK <Johan Kampe>
2021-04-03 13:22:46 +0200

    dmenu_runner: use entry class, use font size from settings.ini
    Signed-off-by: JK <Johan Kampe>

--- dmenu_runner.py
+++ dmenu_runner.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import configparser
-import re
 import subprocess
 import time
 import webbrowser
 from dataclasses import dataclass
 from enum import Enum
+from config import ConfigurationManager
 from pathlib import Path
 
 import dmenu
@@ -20,7 +19,15 @@ class DmenuConfig:
     color_selected_foreground: str = "white"
     color_selected_background: str = "#042C44"
     color_bar_background: str = "black"
+    font: str = "Monospace-{}:normal".format(ConfigurationManager().get("dmenu_font_size", default=10))
     lines: int = 40
+
+
+class WorkspaceName(Enum):
+    GnomeSettings = "settings"
+
+    def __str__(self):
+        return str(self.value)
 
 
 def show_notification(message, title=None, time_ms=1200):
@@ -30,9 +37,9 @@ def show_notification(message, title=None, time_ms=1200):
         subprocess.Popen(['notify-send', message, "-t", str(time_ms)])
 
 
-DMENU_COLORS = {"sf": "red", "sb": "orange", "nb": "black"}
-SOURCE_PENV_CMD = r"source ~/scripts/python-venv/bin/activate.fish"
-GNOME_CTRL_CENTR_CMD = "env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
+def show_notification_error(message):
+    subprocess.Popen(['notify-send', "ERROR:" + str(__file__),
+                      str(message), "-t", str(5000), "-u", "critical"])
 
 
 def i3_exec(command, workspace_name=None, layout=None, sleep_time=1, notify=False):
@@ -54,12 +61,25 @@ def new_terminal(start_command="", workspace_name=None):
     i3_exec(cmd, workspace_name=workspace_name)
 
 
-def dmenu_show(command_list: list):
+def dmenu_show(command_list: list, config: DmenuConfig = None):
+    if config is None:
+        config = DmenuConfig()
     return dmenu.show(command_list,
-                      case_insensitive=DmenuConfig.case_insensitive,
-                      foreground_selected=DmenuConfig.color_selected_foreground,
-                      background=DmenuConfig.color_bar_background,
-                      lines=DmenuConfig.lines)
+                      font=config.font,
+                      case_insensitive=config.case_insensitive,
+                      foreground_selected=config.color_selected_foreground,
+                      background=config.color_bar_background,
+                      lines=config.lines)
+
+
+def dmenu_poweroff():
+    cmd_str_pwr_off = "Power off computer"
+    commands = [cmd_str_pwr_off, "Cancel"]
+    config = DmenuConfig()
+    config.color_selected_foreground = "red"
+    ret = dmenu_show(commands, config)
+    if cmd_str_pwr_off == ret:
+        i3_exec("poweroff")
 
 
 def open_web(url: str):
@@ -74,35 +94,76 @@ def subprocces_popen(cmd: str):
     subprocess.Popen(cmd.split(" "))
 
 
+def get_pycharm_path():
+    bin_path = Path.home() / "bin"
+    for item in bin_path.glob("**/pycharm.sh"):
+        return item
+    return None
+
+
+class Entry:
+    def __init__(self, label, func=None, args=None):
+        if isinstance(label, list):
+            self.labels = label
+        else:
+            self.labels = [label]
+        if func is None:
+            self.funcs = []
+        else:
+            self.funcs = [(func, args)]
+
+    def execute(self):
+        for func, args in self.funcs:
+            if args is None:
+                func()
+            elif isinstance(args, str):
+                func(*args.split(" "))
+            elif type(args) in [list, tuple]:
+                func(*args)
+            elif isinstance(args, dict):
+                func(**args)
+
+    def add_label(self, label):
+        self.labels.append(label)
+
+    def add_func(self, func, args=None, sleep_time_before_exec: int = 0):
+        try:
+            if sleep_time_before_exec > 0:
+                self.funcs.append((time.sleep, [sleep_time_before_exec]))
+            self.funcs.append((func, args))
+        except Exception as _error:
+            show_notification_error("Entry::add_func:\n" + str(_error))
+
+
+def init_entries():
+    ret_list = []
+    ret_list.append(Entry("Firefox", i3_exec, "firefox"))
+    ret_list.append(Entry("Settings",
+                          i3_exec,
+                          {"command": "env XDG_CURRENT_DESKTOP=GNOME gnome-control-center",
+                           "workspace_name": str(WorkspaceName.GnomeSettings)}))
+    ret_list.append(Entry("PowerOff", dmenu_poweroff))
+    pycharm_path = get_pycharm_path()
+    if pycharm_path:
+        ret_list.append(Entry("PyCharm", i3_exec, {"command": pycharm_path}))
+    return ret_list
+
+
+def main():
+    try:
+        entries = init_entries()
+    except Exception as _error:
+        show_notification_error("init_entries:\n" + str(_error))
+        return
+    label_list = []
+    for e in entries:
+        label_list.extend(e.labels)
+    ret = dmenu_show(sorted(label_list, key=str.casefold), config=DmenuConfig())
+    for entry in entries:
+        if ret in entry.labels:
+            entry.execute()
+            break
+
+
 if __name__ == "__main__":
-    COMMANDS = {"Firefox": "firefox",
-                "VsCode": "code",
-                "Lock": "i3lock --c 000000",
-                "Shutdown": "poweroff",
-                "Poweroff": "poweroff",
-                "xkill": "xkill",
-                "Okular": "okular",
-                "PDF Viewer": "okular",
-                "Nautilus": "nautilus",
-                "arandr": "arandr",
-                "Weather": None,
-                "Settings UI": None,
-                }
-    RET = dmenu_show(sorted(COMMANDS.keys(), key=str.casefold))
-    if "Weather" in RET:
-        new_terminal(start_command="curl https://wttr.in/Norrkoping",
-                     workspace_name="wttr.in")
-    elif "ssh " in RET:
-        SERVER = RET.replace('ssh ', '')
-        show_notification(
-            f"connecting to: {SERVER}", title="ssh command")
-        new_terminal(start_command=RET, workspace_name=f"ssh:{SERVER}")
-    elif "google " in RET:
-        QUERY = RET.replace('google ', '')
-        open_web(f"https://www.google.com/search?q={QUERY.replace(' ', '+')}")
-    elif "Settings" in RET:
-        i3_exec(GNOME_CTRL_CENTR_CMD, workspace_name=f"settings")
-    else:
-        RET_OP = COMMANDS.get(RET, None)
-        if RET_OP:
-            i3_exec(RET_OP)
+    main()
