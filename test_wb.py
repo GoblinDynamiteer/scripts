9429c7b758a02aff79afdfa6e6ccbf698739b956
JK <Johan Kampe>
2021-06-19 17:53:16 +0200

    wb_new: list dir/files
    Signed-off-by: JK <Johan Kampe>

---
+++ test_wb.py
@@ -0,0 +1,106 @@
+from wb_new import *
+
+from config import ConfigurationManager
+
+
+class TestFileListItem:
+    def test_is_x(self):
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        assert fi.is_file is False
+        assert fi.is_dir is True
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/smkm-clgrp.rar"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.File)
+        assert fi.is_file is True
+        assert fi.is_dir is False
+
+    def test_parent(self):
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        assert fi.parent() == Path("/home/user/files/")
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/smkm-clgrp.rar"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.File)
+        assert fi.parent() == Path("/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/")
+
+    def test_is_parent_of(self):
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP"
+        fi_parent = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/smkm-clgrp.rar"
+        fi_child = FileListItem(_find_str, item_type=FileListItem.ItemType.File)
+        assert fi_parent.is_parent_of(fi_child) is True
+
+    def test_is_top_level(self, tmp_path):
+        _file = tmp_path / "_settings.txt"
+        with open(_file, "w") as settings_file:
+            settings_file.write("\n".join(["[wb]", "username=donald"]))
+        ConfigurationManager().set_config_file(_file)
+        _find_str = "/home/donald/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        assert fi.is_top_level() is True
+
+    def test_assert_top_dir_is_invalid(self, tmp_path):
+        _file = tmp_path / "_settings.txt"
+        with open(_file, "w") as settings_file:
+            settings_file.write("\n".join(["[wb]", "username=ronald"]))
+        ConfigurationManager().set_config_file(_file)
+        _find_str = "/home/ronald/files/"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        assert fi.valid is False
+
+    def test_valid_file(self):
+        _find_str = "/home/donald/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/smkm-clgrp.rar"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.File)
+        assert fi.valid is True
+        _find_str = "/home/donald/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/smkm-clgrp.nfo"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.File)
+        assert fi.valid is False
+
+    def test_string_scrubbing(self):
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/smkm-clgrp.rar\n "
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.File)
+        assert fi.valid is True
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/smkm-clgrp.r00\n "
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.File)
+        assert fi.valid is False
+
+    def test_parent_invalid(self):
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/Sample/sample.mkv"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.File)
+        assert fi.valid is False
+
+    def test_is_relative_of(self):
+        _base_str = "/home/user/files/Some.Kind.of.Show.S02.1080p.WEB.h264-COOLGRP/"
+        fi_base = FileListItem(_base_str, item_type=FileListItem.ItemType.Dir)
+        _sub_dir_str = _base_str + "Some.Kind.of.Show.S02E01.1080p.WEB.h264-COOLGRP/"
+        fi_sub_dir = FileListItem(_sub_dir_str, item_type=FileListItem.ItemType.Dir)
+        _file_str = _sub_dir_str + "skoss02e01-clgrp.rar"
+        fi_file = FileListItem(_file_str, item_type=FileListItem.ItemType.File)
+        assert fi_sub_dir.is_parent_of(fi_file)
+        assert fi_base.is_relative_of(fi_file)
+
+    def test_str_remove_prefix(self, tmp_path):
+        _file = tmp_path / "_settings.txt"
+        with open(_file, "w") as settings_file:
+            settings_file.write("\n".join(["[wb]", "username=donald"]))
+        ConfigurationManager().set_config_file(_file)
+        _find_str = "/home/donald/files/Some.Kind.of.Show.S02.1080p.WEB.h264-COOLGRP/"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        assert str(fi) == "Some.Kind.of.Show.S02.1080p.WEB.h264-COOLGRP/"
+
+    def test_valid_dir(self):
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        assert fi.valid is True
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/Sample"
+        fi = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        assert fi.valid is False
+
+    def test_sub_items(self):
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP"
+        fi_parent = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        _find_str = "/home/user/files/Some.Kind.of.Movie.2021.1080p.WEB.h264-COOLGRP/smkm-clgrp.rar"
+        fi_sub = FileListItem(_find_str, item_type=FileListItem.ItemType.Dir)
+        fi_parent.add_sub_item(fi_sub)
+        _sub = list(fi_parent.sub_items())
+        assert len(_sub) == 1
+        assert _sub[0] is fi_sub
--- wb_new.py
+++ wb_new.py
@@ -1,16 +1,140 @@
 #!/usr/bin/env python3
 
-from util import BaseLog
+from pathlib import Path
+from typing import List
+from enum import Enum
 
+from util import BaseLog
 from config import ConfigurationManager, SettingSection, SettingKeys
+
 from paramiko.client import SSHClient, AutoAddPolicy
 
 
+class FileListItem:
+    class ItemType(Enum):
+        Dir = "directory"
+        File = "File"
+
+    _ignore_dirs = ["Sample", "Proof", "Subs"]
+    _valid_file_ext = [".mkv", ".rar"]
+
+    def __init__(self, string: str, item_type: ItemType):
+        self._type = item_type
+        self._raw = string.replace("\n", "").strip()
+        self._path = Path(self._raw)
+        self._sub_items = []
+        _user = ConfigurationManager().get(key=SettingKeys.WB_USERNAME, section=SettingSection.WB)
+        self._top_level = Path("/home/") / _user / "files"
+
+    def add_sub_item(self, item):
+        self._sub_items.append(item)
+
+    def sub_items(self):
+        for item in self._sub_items:
+            yield item
+
+    def is_parent_of(self, other: "FileListItem"):
+        if other.is_top_level():
+            return False
+        if self.is_file:
+            return False
+        return self._path == other.parent()
+
+    def is_relative_of(self, other: "FileListItem"):
+        if other.is_top_level():
+            return False
+        if self.is_file:
+            return False
+        try:
+            return other.path.is_relative_to(self._path)
+        except AttributeError as _:
+            pass
+        return str(other).startswith(str(self))
+
+    def is_top_level(self):
+        return self._path.parent == self._top_level
+
+    @property
+    def is_dir(self):
+        return self._type == self.ItemType.Dir
+
+    @property
+    def is_file(self):
+        return self._type == self.ItemType.File
+
+    @property
+    def valid(self):
+        if self.is_dir:
+            if self._path == self._top_level:
+                return False
+            if self._path.name in self._ignore_dirs:
+                return False
+        if self.is_file:
+            if self._path.suffix not in self._valid_file_ext:
+                return False
+            if self.parent().name in self._ignore_dirs:
+                return False
+        return True
+
+    def __str__(self):
+        _ret = str(self._path).replace(f"{self._top_level}/", "")
+        if self.is_dir and not _ret.endswith("/"):
+            return f"{_ret}/"
+        return _ret
+
+    @property
+    def name(self) -> str:
+        return self._path.name
+
+    def parent(self):
+        return self._path.parent
+
+    @property
+    def path(self):
+        return self._path
+
+
 class FileList:
-    def __init__(self, ls_output: list):
-        self._raw = ls_output
-        for line in ls_output:
-            print(line)
+    def __init__(self):
+        self._items: List[FileListItem] = []
+
+    def parse_find_d_output(self, lines: List[str]):
+        for line in lines:
+            self._add_item(FileListItem(line, item_type=FileListItem.ItemType.Dir))
+
+    def parse_find_f_output(self, lines: List[str]):
+        for line in lines:
+            self._add_item(FileListItem(line, item_type=FileListItem.ItemType.File))
+
+    def _add_item(self, item: FileListItem):
+        if not item.valid:
+            return
+        if item.is_top_level():
+            self._items.append(item)
+            return
+        _parent = self._get_parent_item(self._items, item)
+        if _parent is not None:
+            _parent.add_sub_item(item)
+        else:
+            self._items.append(item)
+
+    def _get_parent_item(self, items: List[FileListItem], item: FileListItem) -> [FileListItem, None]:
+        for _item in items:
+            if _item.is_parent_of(item):
+                return _item
+            if _item.is_relative_of(item):
+                return self._get_parent_item(list(_item.sub_items()), item)
+        return None
+
+    def print(self):
+        for item in self._items:
+            self.print_item(item)
+
+    def print_item(self, item: FileListItem, indent=0):
+        prefix = "[DIR]" if item.is_dir else "[FIL]"
+        print(prefix + " " * indent, item.name)
+        for sub_item in item.sub_items():
+            self.print_item(sub_item, indent+4)
 
 
 class SSHConnection(BaseLog):
@@ -35,7 +159,12 @@ class SSHConnection(BaseLog):
         if not self._connected:
             return None
         _, stdout, _ = self._client.exec_command(command)
-        return stdout.readlines()
+        try:
+            return stdout.readlines()
+        except AttributeError as _:
+            if isinstance(stdout, str):
+                return stdout.split("\n")
+        return None
 
     @property
     def connected(self):
@@ -64,7 +193,11 @@ class Server(BaseLog):
         if not self._ssh.connected:
             self.error("cannot retrieve file list, not connected")
             return None
-        return FileList(self._ssh.run_command(r"ls -latr ~/files"))
+        _file_list = FileList()
+        _file_list.parse_find_d_output(self._ssh.run_command(r"find ~/files -type d"))
+        _file_list.parse_find_f_output(self._ssh.run_command(r"find ~/files -type f"))
+        _file_list.print()
+        return _file_list
 
 
 class ServerHandler:
