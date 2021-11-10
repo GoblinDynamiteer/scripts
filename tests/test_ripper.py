6a5109113c8e2c154fc8e0b22fb3a2154fd6aa55
Johan Kampe (Windows) <kampe.johan@gmail.com>
2021-11-10 06:55:59 +0100

    tests: fix tests in test_ripper
    and use tmp_path fixture

--- tests/test_ripper.py
+++ tests/test_ripper.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import tempfile
 import json
 from datetime import datetime
+import argparse
 
 import pytest
 
@@ -34,51 +35,53 @@ DATE_FRIDAY_1900 = datetime(2021, 4, 9, 19, 0, 0)
 DATE_FRIDAY_2100 = datetime(2021, 4, 9, 21, 0, 0)
 
 
-def test_scheduled_show_list_no_existing_file():
-    non_file = Path(tempfile.gettempdir()) / "non_existing_schedule___.json"
+def args_for_scheduled_show_list():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("--simulate", action="store_true")
+    return parser.parse_args(["--verbose", "--simulate"])
+
+
+def test_scheduled_show_list_no_existing_file(tmp_path):
+    non_file = tmp_path / "non_existing_schedule___.json"
     assert not non_file.exists()
-    args = get_cli_args()
+    args = args_for_scheduled_show_list()
     args.json_file = non_file
     show_list = ScheduledShowList(args)
     assert not show_list.valid
     assert show_list.empty()
 
 
-def test_scheduled_show_list():
-    sched_file = Path(tempfile.gettempdir()) / "test_show_schedule.json"
-    with open(sched_file, "w") as _file:
-        json.dump(DATA, _file)
-    args = get_cli_args()
+def test_scheduled_show_list(tmp_path):
+    sched_file = tmp_path / "test_show_schedule.json"
+    sched_file.write_text(json.dumps(DATA))
+    args = args_for_scheduled_show_list()
     args.json_file = sched_file
-    args.verbose = True
     show_list = ScheduledShowList(args)
     assert show_list.valid
     assert len([s for s in show_list]) == 2
     assert not show_list.empty()
 
 
-def test_scheduled_show_list_one_show_next_show():
-    sched_file = Path(tempfile.gettempdir()) / "test_show_schedule.json"
-    with open(sched_file, "w") as _file:
-        json.dump(DATA, _file)
-    args = get_cli_args()
+def test_scheduled_show_list_one_show_next_show(tmp_path):
+    sched_file = tmp_path / "test_show_schedule.json"
+    sched_file.write_text(json.dumps(DATA))
+    args = args_for_scheduled_show_list()
     args.json_file = sched_file
-    args.verbose = True
     show_list = ScheduledShowList(args)
     next_show = show_list.next_show()
     assert next_show.name == "SomeShow"
 
 
-def test_scheduled_show_list_one_show_next_show_seconds_to(mocker):
-    mocker.patch("ripper_scheduler.get_now", return_value=DATE_FRIDAY_1900)
-    sched_file = Path(tempfile.gettempdir()) / "test_show_schedule.json"
-    with open(sched_file, "w") as _file:
-        json.dump(DATA, _file)
-    args = get_cli_args()
+def test_scheduled_show_list_one_show_next_show_seconds_to(mocker, tmp_path):
+    _gn = mocker.patch("ripper_scheduler.get_now", return_value=DATE_FRIDAY_1900)
+    sched_file = tmp_path / "test_show_schedule.json"
+    sched_file.write_text(json.dumps(DATA))
+    args = args_for_scheduled_show_list()
     args.json_file = sched_file
-    args.verbose = True
     show_list = ScheduledShowList(args)
     next_show = show_list.next_show()
+    _gn.assert_called()
     assert not next_show.should_download()
     assert next_show.shortest_airtime() == 60 * 60
 
