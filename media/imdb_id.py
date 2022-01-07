28334667d5bb6455bc2b54d1c821cfe623c759a5
Johan Kampe (Windows) <kampe.johan@gmail.com>
2022-01-07 08:54:36 +0100

    media/imdb_id,tests: add id parsing of paths: dirs and files

--- media/imdb_id.py
+++ media/imdb_id.py
@@ -19,16 +19,45 @@ class IMDBId(BaseLog):
         if isinstance(value, str):
             self._parse_str(value)
         elif isinstance(value, Path):
-            pass
+            if value.is_file():
+                self._parse_file(value)
+            elif value.is_dir():
+                self._parse_dir(value)
+            else:
+                raise FileNotFoundError(f"{value} does not exist!")
+        else:
+            raise TypeError(f"invalid type of value ({value}): {type(value)}!")
 
-    def _parse_str(self, string: str) -> None:
+    def _parse_file(self, file_path: Path):
+        if file_path.suffix not in [".nfo", ".txt"]:  # TODO: add better check, how to mimic ("file xxx" command?)
+            self.warn(f"processing (possibly) non-text/ascii file: {file_path}")
+        if file_path.stat().st_size > 1024 * 10:  # TODO: what is a reasonable file size for nfo files?
+            self.warn(f"processing \"large\" file: {file_path}")
+        _found = False
+        with open(file_path, "r") as fp:
+            for _line in fp.readlines():
+                if self._parse_str(_line):
+                    _found = True
+        if _found:
+            self.log(f"found id in file: {file_path}")
+
+    def _parse_dir(self, dir_path: Path):
+        for _item in dir_path.iterdir():
+            if _item.is_file() and _item.suffix in [".nfo", ".txt"]:
+                self._parse_file(_item)
+
+    def _parse_str(self, string: str) -> bool:
         matches = re.findall(self.REGEX, string)
         if not matches:
-            return
+            return False
+        _ret = False
         for match in matches:
             _num = match.lower().replace("tt", "")
             if _num not in self._ids:
+                self.log(f"found id: {_num}")
                 self._ids.append(_num)
+                _ret = True
+        return _ret
 
     def has_multiple_ids(self) -> bool:
         return len(self._ids) > 1
@@ -46,8 +75,9 @@ def main():
     import argparse
     parser = argparse.ArgumentParser("IMDbId Helper")
     parser.add_argument("string")
+    parser.add_argument("--path", action="store_true", help="pass as path")
     args = parser.parse_args()
-    iid = IMDBId(args.string)
+    iid = IMDBId(Path(args.string) if args.path else args.string)
     print(iid)
 
 
--- tests/test_imdb_id.py
+++ tests/test_imdb_id.py
@@ -1,8 +1,13 @@
+from typing import List
+from pathlib import Path
+
+import pytest
+
 from media.imdb_id import IMDBId
 
 
-class TestIMDBId:
-    def test_string_parse_single(self):
+class TestIMDBIdStringParse:
+    def test_single(self):
         iid = IMDBId("https://www.imdb.com/title/tt9174578/")
         assert str(iid) == "tt9174578"
         assert iid.has_multiple_ids() is False
@@ -12,22 +17,93 @@ class TestIMDBId:
         assert iid.has_multiple_ids() is False
         assert iid.valid() is True
 
-    def test_string_parse_multiple(self):
+    def test_multiple(self):
         iid = IMDBId("texttext https://www.imdb.com/title/tt9174578/"
                      " texttexttext https://www.imdb.com/title/tt13315308/ x")
         assert str(iid) == "tt9174578"
         assert iid.has_multiple_ids() is True
         assert iid.valid() is True
 
-    def test_string_parse_multiple_same_id(self):
+    def test_multiple_same_id(self):
         iid = IMDBId("texttext https://www.imdb.com/title/tt9174578/"
                      " texttexttext https://www.imdb.com/title/tt9174578/ x")
         assert str(iid) == "tt9174578"
         assert iid.has_multiple_ids() is False
         assert iid.valid() is True
 
-    def test_string_parse_invalid(self):
+    def test_invalid(self):
         iid = IMDBId("something else")
         assert str(iid) == ""
         assert iid.has_multiple_ids() is False
         assert iid.valid() is False
+
+
+class TestIMDBIdPathParse:
+    def test_movie_nfo(self, tmp_path):
+        _dirs = tmp_path / "movies" / "M"
+        _dirs.mkdir(parents=True)
+        _file_mov = _dirs / "Movie.2015.1080p.WEB-DL.DD5.1.H264-Grp.mkv"
+        _file_mov.touch()
+        _file_nfo = _dirs / "movie.nfo"
+        with open(_file_nfo, "w") as fp:
+            fp.write("tt2782868")
+        assert _file_nfo.is_file()
+        iid = IMDBId(_file_nfo)
+        assert str(iid) == "tt2782868"
+        assert iid.has_multiple_ids() is False
+        assert iid.valid() is True
+
+    def test_movie_path_containing_nfo(self, tmp_path):
+        _dirs = tmp_path / "movies" / "M"
+        _dirs.mkdir(parents=True)
+        _file_mov = _dirs / "Movie.2015.1080p.WEB-DL.DD5.1.H264-Grp.mkv"
+        _file_mov.touch()
+        _file_nfo = _dirs / "movie.nfo"
+        with open(_file_nfo, "w") as fp:
+            fp.write("tt5834426")
+        assert _file_nfo.is_file()
+        iid = IMDBId(_dirs)
+        assert str(iid) == "tt5834426"
+        assert iid.has_multiple_ids() is False
+        assert iid.valid() is True
+
+    def test_release_nfo(self, tmp_path):
+        _base: str = "movie.2021.1080p.bluray.x264-grp"
+        _mp: Path = tmp_path / "Movie.2021.1080p.BluRay.x264-Grp"
+        _mp.mkdir()
+        nfo_contents: List[str] = ["Name: Movie.2021.1080p.BluRay.x264-Grp",
+                                   "",
+                                   "Release date: 04-01-2022",
+                                   "",
+                                   "IMDB link: http://www.imdb.com/title/tt7740510",
+                                   "",
+                                   "Resolution: 1920x1036",
+                                   "Audio: English DTS-HD Master 6ch 3821 kbps",
+                                   "Video: 7750 kbps, CRF 15.5",
+                                   "Source: 35970 kbps",
+                                   "Subtitles: English SDH, French, Spanish"]
+        for ext in ["rar", "sfv"] + [f"r{n:02d}" for n in range(90)]:
+            _f = _mp / f"{_base}.{ext}"
+            _f.touch()
+        with open(_mp / f"{_base}.nfo", "w") as fp:
+            fp.writelines(nfo_contents)
+        iid = IMDBId(_mp)
+        assert str(iid) == "tt7740510"
+        assert iid.has_multiple_ids() is False
+        assert iid.valid() is True
+
+
+class TestIMDBIdExceptions:
+    def test_pass_incorrect_type(self):
+        with pytest.raises(TypeError):
+            IMDBId(999)  # TODO: this should possibly be valid...
+        with pytest.raises(TypeError):
+            IMDBId(123.123)
+        with pytest.raises(TypeError):
+            IMDBId(IMDBId("http://www.imdb.com/title/tt7740510"))
+
+    def test_pass_non_existing_path(self):
+        _p = Path("/Nonexisting/Path/")
+        assert _p.exists() is False
+        with pytest.raises(FileNotFoundError):
+            IMDBId(_p)
