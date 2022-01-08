a8784540a1ba0792d9d696711592a54ad7788936
Johan Kampe (Windows) <kampe.johan@gmail.com>
2022-01-08 11:23:58 +0100

    omdb: use MovieData or IMDBId objects as input
    for search

--- omdb.py
+++ omdb.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
+from typing import Dict, Optional, Union
 import json
-import pprint
 import urllib.parse
 import urllib.request
 from argparse import ArgumentParser
 
 import util
 from base_log import BaseLog
-from singleton import Singleton
-from config import ConfigurationManager
+from config import ConfigurationManager, SettingKeys
+from media.movie import MovieData
+from media.imdb_id import IMDBId
 
 
 class OMDbMovieSearchResult:
@@ -53,8 +54,7 @@ class OMDb(BaseLog):
     def __init__(self, verbose=False):
         BaseLog.__init__(self, verbose=verbose)
         self.set_log_prefix("OMDb")
-        self._api_key = ConfigurationManager().get("omdb_api_key", default=None)
-        self._url_args = {"apikey": self._api_key, "type": "movie"}
+        self._api_key: Optional[str] = ConfigurationManager().get(SettingKeys.API_KEY_OMDB, assert_exists=True)
         self.log("init")
 
     @property
@@ -62,27 +62,45 @@ class OMDb(BaseLog):
         return self._api_key is not None
 
     def _search(self, url):
-        try:
-            _res = urllib.request.urlopen(url, timeout=4).read().decode("utf-8")
-            return json.loads(_res)
-        except:
-            pass
-        return {}
+        self.log_fs(f"searching... url: i[{url}]")
+        _res = urllib.request.urlopen(url, timeout=4).read().decode("utf-8")
+        return json.loads(_res)
 
-    def movie_search(self, title=None, imdb_id=None, year=None):
-        if not title and not imdb_id:
-            return OMDbMovieSearchResult({})
-        if util.is_imdbid(imdb_id):
-            self._url_args["i"] = util.parse_imdbid(imdb_id)
-        elif title:
-            self._url_args["t"] = title
-        if util.is_valid_year(year):
-            self._url_args["y"] = year
-        url = f"{self.URL}?{urllib.parse.urlencode(self._url_args)}"
-        for key in ["i", "y", "t"]:
-            self._url_args.pop(key, None)
+    def _args(self) -> Dict[str, str]:
+        return {"apikey": self._api_key, "type": "movie"}
+
+    def _url_from_imdb(self, imdb_id: IMDBId) -> Optional[str]:
+        if not imdb_id.valid():
+            self.error("IMDbId invalid, cannot build search request")
+            return None
+        _args = self._args()
+        _args["i"] = str(imdb_id)
+        return f"{self.URL}?{urllib.parse.urlencode(_args)}"
+
+    def _url_from_movie_data(self, data: MovieData) -> Optional[str]:
+        year, title = data.year, data.title
+        if not title:
+            self.error("title is required, cannot build search request")
+            return None
+        _args = self._args()
+        _args["t"] = title
+        if year:
+            _args["y"] = str(year)
+        return f"{self.URL}?{urllib.parse.urlencode(_args)}"
+
+    def movie_search(self, data: Union[MovieData, IMDBId]) -> Optional[OMDbMovieSearchResult]:
+        if not data:
+            self.error("search requires either MovieData or IMDBId to be set")
+            return None
+        if isinstance(data, IMDBId):
+            url = self._url_from_imdb(data)
+        elif isinstance(data, MovieData):
+            url = self._url_from_movie_data(data)
+        else:
+            self.error(f"invalid search data type: {type(data)}")
+            return None
         _existing = self._results.get(url, None)
-        if _existing:
+        if _existing is not None:
             return _existing
         _ret = OMDbMovieSearchResult(self._search(url))
         self._results[url] = _ret
@@ -96,9 +114,16 @@ def main():
     grp.add_argument("--id", "-i", type=str, default=None)
     grp.add_argument("--title", "-t", type=str, default=None)
     args = parser.parse_args()
-    pp = pprint.PrettyPrinter(indent=4)
     _omdb = OMDb()
-    pp.pprint(_omdb.movie_search(title=args.title, year=args.year, imdb_id=args.id))
+    if args.title:
+        data = MovieData(title=args.title, year=args.year)
+    else:
+        data = IMDBId(args.id)
+    res: Optional[OMDbMovieSearchResult] = _omdb.movie_search(data)
+    if res is None or not res.valid:
+        print("could not get a valid search result!")
+    else:
+        res.print()
 
 
 if __name__ == "__main__":
