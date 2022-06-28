e4a25d108e204df4732cb892926c4d2b0495d863
Johan Kampe <kampe.johan@gmail.com>
2022-06-28 17:14:12 +0200

    scan: add scan_types arg
    to choose what shall be scanned.
    if omitted, all types will be scanned

--- scan.py
+++ scan.py
@@ -1,11 +1,16 @@
 from argparse import ArgumentParser, Namespace
+from typing import List, Callable
 
 from media.scan.show import ShowScanner
 from media.scan.movie import MovieScanner
 
+SCAN_TYPE_MOV = ("mov", "movie", "film")
+SCAN_TYPE_TV = ("tv", "show", "ep", "episodes")
+
 
 def get_args() -> Namespace:
     parser = ArgumentParser("media scanner")
+    parser.add_argument("scan_types", default=None, nargs="*")
     parser.add_argument("--verbose", "-v",
                         action="store_true",
                         help="be verbose")
@@ -15,22 +20,48 @@ def get_args() -> Namespace:
     return parser.parse_args()
 
 
-def main():
-    args = get_args()  # TODO: use simulate from args
-    mov_scan = MovieScanner(update_database=False, verbose=args.verbose)
+def scan_movies(args: Namespace) -> None:
+    mov_scan = MovieScanner(update_database=not args.simulate,
+                            verbose=args.verbose)
     print("Scanning for new movies...")
     count = mov_scan.scan()
     if count == 0:
         print("-> No new movies found")
     else:
         print(f"-> Found {count} new movie(s)")
-    show_scan = ShowScanner(update_database=False, verbose=args.verbose)
+
+
+def scan_shows(args: Namespace) -> None:
+    show_scan = ShowScanner(update_database=not args.simulate,
+                            verbose=args.verbose)
     print("Scanning for new episodes...")
     count = show_scan.scan()
     if count == 0:
         print("-> No new episodes found")
     else:
         print(f"-> Found {count} new episode(s)")
+
+
+def _args_to_funcs(args: Namespace) -> List[Callable[[Namespace], None]]:
+    if not isinstance(args.scan_types, list):
+        return [scan_movies, scan_shows]
+    _ret = []
+    for scan_type in args.scan_types:
+        if scan_type in SCAN_TYPE_TV:
+            _ret.append(scan_shows)
+        elif scan_type in SCAN_TYPE_MOV:
+            _ret.append(scan_movies)
+        elif args.verbose:
+            print(f"invalid scan type: {scan_type}")
+    if not _ret and args.verbose:
+        print("could not determine scan types...")
+    return list(set(_ret))
+
+
+def main():
+    args = get_args()
+    for _func in _args_to_funcs(args):
+        _func(args)
 
 
 if __name__ == "__main__":
