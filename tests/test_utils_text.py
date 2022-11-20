79e5a07a65659b71603ae5b2bc82fdeace81441d
Johan Kampe (Windows) <kampe.johan@gmail.com>
2022-11-20 22:56:38 +0100

    utils/text_utils,tests: support multiple values in parse_percentage_from_string

--- tests/test_utils_text.py
+++ tests/test_utils_text.py
@@ -22,3 +22,18 @@ class TestTextUtils:
             _str = f"xxxx {p} % yyyy"
             assert parse_percentage_from_string(_str, return_string=True) == f"{p}%"
 
+    def test_parse_percentage_from_string_multiple_get_highest(self):
+        _str = f"xxxx 80 % 99 % yyyy"
+        assert parse_percentage_from_string(_str) == 99
+
+    def test_parse_percentage_from_string_multiple_get_lowest(self):
+        _str = f"xxxx 80 % 99 % yyyy"
+        assert parse_percentage_from_string(_str, get_highest_found=False) == 80
+
+    def test_parse_percentage_from_string_multiple_get_highest_to_str(self):
+        _str = f"xxxx 80 % 99 % yyyy"
+        assert parse_percentage_from_string(_str, return_string=True) == "99%"
+
+    def test_parse_percentage_from_string_multiple_get_lowest_to_str(self):
+        _str = f"xxxx 80 % 99 % yyyy"
+        assert parse_percentage_from_string(_str, return_string=True, get_highest_found=False) == "80%"
--- utils/text_utils.py
+++ utils/text_utils.py
@@ -1,16 +1,23 @@
 import re
-from typing import Optional, Union
+from typing import Optional, Union, List
 
 
-def parse_percentage_from_string(string: str, return_string: bool = False) -> Optional[Union[str, int]]:
+def parse_percentage_from_string(string: str,
+                                 return_string: bool = False,
+                                 get_highest_found: bool = True) -> Optional[Union[str, int]]:
     """Parse a percentage value from a string, if possible"""
+    _ints: List[int] = []
     for regex in (r"\d{1,3}%", r"\d{1,3}.%"):
-        _match = re.search(regex, string)
-        if not _match:
+        _matches = re.findall(regex, string)
+        if not _matches:
             continue
-        _pstr = _match.group().replace("%", " ").strip()
-        if _pstr.isdigit():
-            if return_string:
-                return f"{int(_pstr)}%"
-            return int(_pstr)
-    return None
+        for _m in _matches:
+            _pstr = _m.replace("%", " ").strip()
+            if _pstr.isdigit():
+                _ints.append(int(_pstr))
+    if not _ints:
+        return None
+    _ret_int: int = _ints[0] if not get_highest_found else _ints[-1]
+    if return_string:
+        return f"{int(_ret_int)}%"
+    return _ret_int
